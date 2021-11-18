57e8b3aa38e7654452045e4f742218b08da58774
AlejandroMarts <L16180937@slp.tecnm.mx>
2021-11-17 20:29:55 -0600

    Base de datos

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,5 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        $this->call(LibroSeeder::class);
+
     }
 }
